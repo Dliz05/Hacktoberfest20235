5170b3b935a144952d210799103b4de8d311da85
Aliza Dixit <alizadixit6@gmail.com>
2023-10-11 13:43:04 +0530

    Update Kadanes.cpp
    scalar appraoch

--- Project/Cpp Progs/Kadanes.cpp
+++ Project/Cpp Progs/Kadanes.cpp
@@ -29,3 +29,29 @@ cin>>n;
 	cout << "Maximum contiguous sum is " << max_sum;
 	return 0;
 }
+
+
+##Scalar approach
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a[]={2, -4, 3, -1, 2};
+    int local_max = 0;
+    int global_max = INT_MIN;
+    int n=sizeof(a)/sizeof(a[0]);
+    
+    for(i = 0; i < n;i++)
+	{   
+	    local_max = max(a[i], a[i] + local_max);
+        
+	    if(local_max > global_max)
+	    {
+	        global_max = local_max; 
+	    }
+	    
+	}
+	cout<<global_max;
+	return 0;
+}
